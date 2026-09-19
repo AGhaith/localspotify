b7e259a3ab35d8879bde26bca2e66b0d4dc5f3cf
Ahmed Ghaith <a.wesam2300@nu.edu.eg>
2026-09-19 23:47:31 +0300

    fix(android): configure compileSdk 36, AudioServiceActivity, and non-incremental kotlin build

--- mobile_app/android/app/src/main/kotlin/com/localspotify/app/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/localspotify/app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.localspotify.app
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: AudioServiceActivity() {
 }
